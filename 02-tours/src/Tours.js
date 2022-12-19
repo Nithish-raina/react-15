78c0c35fb40aede67a1bfd0ac07c740dc5192cd2
Nithish-maker <nithishraina0323@gmail.com>
2022-12-19 14:52:57 +0530

    project 2 deployed

---
+++ 02-tours/src/Tours.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour 
+                  key = {tour.id} 
+                  id = {tour.id} 
+                  image = {tour.image}
+                  info = {tour.info} 
+                  name = {tour.name}
+                  price = {tour.price}       
+                  removeTour={removeTour} 
+                  />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
