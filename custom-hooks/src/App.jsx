78933e5e1af79db06bb7d4f72eaf28a867fd73f7
Nithish-Lakshmanan <nithish@agentz.ai>
2023-10-28 15:11:24 +0530

    feat(new): added custom hook snippets

---
+++ custom-hooks/src/App.jsx
@@ -0,0 +1,143 @@
+/* eslint-disable no-unused-vars */
+import React, { useReducer } from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+import Posts from "./Posts";
+
+function App() {
+    const [count, setCount] = useState(0);
+    const myRandom = () => {
+        console.log("Calling myy random function");
+        return Math.random();
+    };
+    const logUsers = (name) => {
+        // do something
+        console.log(`Hello ${name}`);
+    };
+    // useEffect(() => {
+    //     const clearIntId = setInterval(() => logUsers("nithish"), 2000);
+    //     return () => {
+    //         clearInterval(clearIntId);
+    //     };
+    // });
+    const [no, setNumber] = useState(() => myRandom());
+    const incrementCount = () => {
+        setCount((prevCount) => prevCount + 1);
+        // setCount((prevCount) => prevCount + 1);
+    };
+    const handleChangeRandomNumber = () => {
+        setNumber(Math.random());
+    };
+    // if (1) {
+    //     console.log("Inside if block");
+    //     const [data, setData] = useState({});
+    // }
+    const useOfAmperSandOperator = (
+        <>
+            {0 && <div> 0 is Rendering </div>}
+            {undefined && <div> undefined is Rendering </div>}
+            {null && <div> null is Rendering </div>}
+            {false && <div> false is Rendering </div>}
+            {NaN && <div>NaN is Rendering </div>}
+        </>
+    );
+    const people = [
+        {
+            id: 0,
+            name: "Creola Katherine Johnson",
+            profession: "mathematician",
+        },
+        {
+            id: 1,
+            name: "Mario José Molina-Pasquel Henríquez",
+            profession: "chemist",
+        },
+        {
+            id: 2,
+            name: "Mohammad Abdus Salam",
+            profession: "physicist",
+        },
+        {
+            name: "Percy Lavon Julian",
+            profession: "chemist",
+        },
+        {
+            name: "Subrahmanyan Chandrasekhar",
+            profession: "astrophysicist",
+        },
+    ];
+    const peopleList = people.map(
+        ({ name: personName, profession: personProfession }) => {
+            return (
+                <li key={personName}>
+                    <input value={personName} />
+                </li>
+            );
+        }
+    );
+    const refVal = React.createRef();
+    const changeRefVal = () => {
+        console.log(refVal);
+        refVal.current = Math.floor(Math.random() * 10);
+    };
+    const doAsyncOperation = () => {
+        return new Promise((resolve, reject) => {
+            resolve("PROMISE RESOLVED");
+        });
+    };
+    const [name, setName] = useState("");
+    useEffect(() => {
+        doAsyncOperation().then(
+            (data) => {
+                try {
+                    setName(data);
+                } catch (error) {
+                    console.log("Error in setting name state", error);
+                }
+            },
+            (error) => console.log("ERROR OCCURED", error)
+        );
+        // setName(result);
+    });
+    const userDetailsReducer = (state, action) => {
+        console.log(
+            ` User details reducer state here ${state} and action here ${action}`
+        );
+        const result = { ...state, ...action };
+        console.log("RESULT IS ", result);
+        return result;
+        // return "My own string";
+    };
+    const [userDetails, setUserDetails] = useReducer(userDetailsReducer, {
+        name: "",
+        age: "",
+    });
+    return (
+        <>
+            {/* <h1>Use-State-Debugging {no}</h1>
+            <button onClick={handleChangeRandomNumber}> Change number </button>
+            <div className="card">
+                <button onClick={incrementCount}>count is {count}</button>
+            </div>
+            {false && useOfAmperSandOperator}
+            <ul>{peopleList}</ul>
+            <h2>Ref value {refVal.current}</h2>
+            <button onClick={changeRefVal}>Change Ref Value</button>
+            <h3>Hello {name}</h3>
+            <h5>
+                User name {userDetails.name}{" "}
+                <span>User age {userDetails.age}</span>
+                <button
+                    onClick={() =>
+                        setUserDetails({ name: "new name", age: "new age" })
+                    }
+                >
+                    Set user Details{" "}
+                </button>
+            </h5> */}
+            <Posts />
+        </>
+    );
+}
+
+export default App;
---
+++ custom-hooks/src/Posts.jsx
@@ -0,0 +1,80 @@
+import { useCallback, useEffect, useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const asyncReducer = (state, action) => {
+    // console.log("ACTION HERE", action);
+    switch (action.type) {
+        case "pending":
+            return {
+                ...state,
+                status: "pending",
+            };
+        case "resolved":
+            return {
+                ...state,
+                status: "resolved",
+                data: action.datas,
+            };
+        case "rejected":
+            return {
+                ...state,
+                status: "rejected",
+                error: action.error,
+            };
+        default:
+            throw new Error(`Unhandled action type ${action.type}`);
+    }
+};
+// Either memoize the asyncCallBack or importing the asyncCallback ensures stability of the function across every render which is not we wanted to do
+const useAsync = (initialState = {}, asyncCallBack) => {
+    const [state, dispatch] = useReducer(asyncReducer, initialState);
+    useEffect(() => {
+        dispatch({ type: "pending" });
+        const promise = asyncCallBack();
+        promise
+            .then((postsData) => {
+                return postsData.json();
+            })
+            .then((res) => {
+                // console.log(data);
+                dispatch({ type: "resolved", datas: [...res.todos] });
+                // setPosts((prevPosts) => [...prevPosts, ...data.todos]);
+            })
+            .catch((er) => {
+                // console.log("ERROR", er.message);
+                dispatch({ type: "rejected", error: er.message });
+                // setError(error);
+            });
+    }, [asyncCallBack]);
+    return state;
+};
+
+const Posts = () => {
+    const initialState = {
+        status: "pending",
+        data: [],
+        error: null,
+    };
+    const memoizedCallBack = useCallback(() => {
+        return fetch("https://dummyjson.com/todos?limit=3&skip=10");
+    }, []);
+    const { status, data, error } = useAsync(initialState, memoizedCallBack);
+    const postsList = data.map((post) => {
+        const idForItem = uuidv4();
+        return <li key={idForItem}> {post.todo} </li>;
+    });
+
+    if (status === "pending") {
+        return <h2>Fetching posts</h2>;
+    } else if (status === "resolved") {
+        return (
+            <>
+                <h1>Posts list</h1>
+                <ul> {postsList}</ul>
+            </>
+        );
+    } else if (status === "rejected") {
+        return <p>Oops ran out !!! {error}</p>;
+    }
+};
+export default Posts;
